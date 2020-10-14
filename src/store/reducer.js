1304c78715a9558edafb1ccc8b899a32b1f3c78d
TeemourA <taxprogramming@gmail.com>
2020-10-14 23:04:21 +0300

    add redux & basic dispatching

---
+++ src/store/reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  // if (action.type === 'INCREMENT') {
+  //   return {
+  //     ...state,
+  //     counter: state.counter + 1,
+  //   }
+  // }
+  switch (action.type) {
+    case 'INCREMENT':
+      return {...state, counter: state.counter + 1}
+    case 'DECREMENT':
+      return {...state, counter: state.counter - 1}
+    case 'ADD':
+      return {...state, counter: state.counter + action.value}
+    case 'SUBTRACT':
+      return {...state, counter: state.counter - action.value}
+    default:
+      return state;
+  }
+};
+
+export default reducer;
