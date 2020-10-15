507fa79065ef41e178167f319174e6022f039962
TeemourA <taxprogramming@gmail.com>
2020-10-15 20:22:18 +0300

    separate global reducer for few parts

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,29 +1,19 @@
+import actionTypes from '../actions';
+
 const initialState = {
   counter: 0,
-  results: [],
 };
 
 const reducer = (state = initialState, action) => {
-  // if (action.type === 'INCREMENT') {
-  //   return {
-  //     ...state,
-  //     counter: state.counter + 1,
-  //   }
-  // }
   switch (action.type) {
-    case 'INCREMENT':
+    case actionTypes.INCREMENT:
       return { ...state, counter: state.counter + 1 }
-    case 'DECREMENT':
+    case actionTypes.DECREMENT:
       return { ...state, counter: state.counter - 1 }
-    case 'ADD':
+    case actionTypes.ADD:
       return { ...state, counter: state.counter + action.value }
-    case 'SUBTRACT':
+    case actionTypes.SUBTRACT:
       return { ...state, counter: state.counter - action.value }
-    case 'STORE_RESULT':
-      return { ...state, results: state.results.concat({
-        id: new Date(),
-        value: state.counter,
-      }) }
     default:
       return state;
   }
---
+++ src/store/reducers/result.js
@@ -0,0 +1,19 @@
+import actionTypes from '../actions';
+
+const initialState = {
+  results: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return { ...state, results: state.results.concat({ id: new Date(), value: action.counter }) }
+    case actionTypes.DELETE_RESULT:
+      const updatedResults = state.results.filter(result => result.id !== action.resultId);
+      return { ...state, results: updatedResults }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
